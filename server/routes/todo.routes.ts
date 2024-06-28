9a8c145777b6dd34fa0e781a2474106cc719c239
umaidashi <umaidashi18@gmail.com>
2024-06-28 13:05:18 +0900

    feat: export TODO_STATUS

--- server/routes/todo.routes.ts
+++ server/routes/todo.routes.ts
@@ -1,95 +1,96 @@
-import { Elysia, t } from 'elysia'
-import type { NewTodo } from '../db/drizzle/schema'
-import { todoCreateDTO, todoDTO } from '../dto/todo.dto'
-import { todoServices } from '../services/todos'
+import { Elysia, t } from "elysia";
+import type { NewTodo } from "../db/drizzle/schema";
+import { todoCreateDTO, todoDTO } from "../dto/todo.dto";
+import { TODO_STATUS } from "../model/todo.model";
+import { todoServices } from "../services/todos";
 
-export default new Elysia({ name: 'todo' }).group('/todos', app =>
-  app
-    .get('/', async () => await todoServices.list(), {
-      detail: {
-        description: 'List all todos',
-        tags: ['Todos'],
-      },
-      response: {
-        200: t.Array(todoDTO),
-      },
-    })
-    .get(
-      '/:id',
-      async ({ params }) => {
-        const { id } = params
-        const res = await todoServices.one(Number(id))
-        return res
-      },
-      {
-        detail: {
-          description: 'Get a todo by id',
-          tags: ['Todos'],
-        },
-        params: t.Object({
-          id: t.Number(),
-        }),
-        response: {
-          200: todoDTO,
-        },
-      },
-    )
-    .post(
-      '/',
-      async ({ body }) => {
-        const newTodo: NewTodo = { ...body, status: TODO_STATUS.PENDING }
-        const res = await todoServices.create(newTodo)
-        return res
-      },
-      {
-        detail: {
-          description: 'Create a new todo',
-          tags: ['Todos'],
-        },
-        body: todoCreateDTO,
-        response: {
-          201: todoDTO,
-        },
-      },
-    )
-    .post(
-      '/:id/active',
-      async ({ params }) => {
-        const { id } = params
-        const res = await todoServices.activate(Number(id))
-        return res
-      },
-      {
-        detail: {
-          description: 'Mark a todo as Active',
-          tags: ['Todos'],
-        },
-        params: t.Object({
-          id: t.Number(),
-        }),
-        response: {
-          200: todoDTO,
-        },
-      },
-    )
-    .post(
-      '/:id/complete',
-      async ({ params }) => {
-        const { id } = params
-        const res = await todoServices.complete(Number(id))
-        return res
-      },
-      {
-        detail: {
-          description: 'Mark a todo as Complete',
-          tags: ['Todos'],
-        },
-        params: t.Object({
-          id: t.Number(),
-        }),
-        response: {
-          200: todoDTO,
-        },
-      },
-    ),
-)
+export default new Elysia({ name: "todo" }).group("/todos", (app) =>
+	app
+		.get("/", async () => await todoServices.list(), {
+			detail: {
+				description: "List all todos",
+				tags: ["Todos"],
+			},
+			response: {
+				200: t.Array(todoDTO),
+			},
+		})
+		.get(
+			"/:id",
+			async ({ params }) => {
+				const { id } = params;
+				const res = await todoServices.one(Number(id));
+				return res;
+			},
+			{
+				detail: {
+					description: "Get a todo by id",
+					tags: ["Todos"],
+				},
+				params: t.Object({
+					id: t.Number(),
+				}),
+				response: {
+					200: todoDTO,
+				},
+			},
+		)
+		.post(
+			"/",
+			async ({ body }) => {
+				const newTodo: NewTodo = { ...body, status: TODO_STATUS.PENDING };
+				const res = await todoServices.create(newTodo);
+				return res;
+			},
+			{
+				detail: {
+					description: "Create a new todo",
+					tags: ["Todos"],
+				},
+				body: todoCreateDTO,
+				response: {
+					201: todoDTO,
+				},
+			},
+		)
+		.post(
+			"/:id/active",
+			async ({ params }) => {
+				const { id } = params;
+				const res = await todoServices.activate(Number(id));
+				return res;
+			},
+			{
+				detail: {
+					description: "Mark a todo as Active",
+					tags: ["Todos"],
+				},
+				params: t.Object({
+					id: t.Number(),
+				}),
+				response: {
+					200: todoDTO,
+				},
+			},
+		)
+		.post(
+			"/:id/complete",
+			async ({ params }) => {
+				const { id } = params;
+				const res = await todoServices.complete(Number(id));
+				return res;
+			},
+			{
+				detail: {
+					description: "Mark a todo as Complete",
+					tags: ["Todos"],
+				},
+				params: t.Object({
+					id: t.Number(),
+				}),
+				response: {
+					200: todoDTO,
+				},
+			},
+		),
+);
--- server/services/todos/activate/index.ts
+++ server/services/todos/activate/index.ts
@@ -1,24 +1,29 @@
-import { eq } from 'drizzle-orm'
-import { db } from '../../../db/drizzle'
-import { todos } from '../../../db/drizzle/schema'
+import { eq } from "drizzle-orm";
+import { db } from "../../../db/drizzle";
+import { todos } from "../../../db/drizzle/schema";
+import { TODO_STATUS } from "../../../model/todo.model";
 
 export default async function (id: number) {
-  const [todo] = await db.select().from(todos).where(eq(todos.id, id)).execute()
+	const [todo] = await db
+		.select()
+		.from(todos)
+		.where(eq(todos.id, id))
+		.execute();
 
-  if (!todo) {
-    throw new Error('Todo not found')
-  }
+	if (!todo) {
+		throw new Error("Todo not found");
+	}
 
-  const [res] = await db
-    .update(todos)
-    .set({ status: TODO_STATUS.ACTIVE })
-    .where(eq(todos.id, id))
-    .returning({
-      id: todos.id,
-      title: todos.title,
-      description: todos.description,
-      status: todos.status,
-    })
-    .execute()
-  return res
+	const [res] = await db
+		.update(todos)
+		.set({ status: TODO_STATUS.ACTIVE })
+		.where(eq(todos.id, id))
+		.returning({
+			id: todos.id,
+			title: todos.title,
+			description: todos.description,
+			status: todos.status,
+		})
+		.execute();
+	return res;
 }
