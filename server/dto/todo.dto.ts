230e174687042d8bb0be7193522cf048bc3036ad
umaidashi <umaidashi18@gmail.com>
2024-06-30 14:12:25 +0900

    fix: lint

--- server/dto/todo.dto.ts
+++ server/dto/todo.dto.ts
@@ -1,24 +1,24 @@
-import { t } from "elysia";
+import { t } from 'elysia'
 
 export const todoDTO = t.Object({
-	id: t.Number(),
-	title: t.String(),
-	description: t.String(),
-	status: t.String(),
-});
+  id: t.Number(),
+  title: t.String(),
+  description: t.String(),
+  status: t.String(),
+})
 
 export const todoCreateDTO = t.Object({
-	title: t.String(),
-	description: t.String(),
-});
+  title: t.String(),
+  description: t.String(),
+})
 
 export const todoReqIdDTO = t.Object({
-	id: t.Numeric(),
-});
+  id: t.Numeric(),
+})
 
 export const todoSearchQueryDTO = t.Optional(
-	t.Object({
-		title: t.Optional(t.String()),
-		status: t.Optional(t.String()),
-	}),
-);
+  t.Object({
+    title: t.Optional(t.String()),
+    status: t.Optional(t.String()),
+  }),
+)
--- server/services/todos/search/index.ts
+++ server/services/todos/search/index.ts
@@ -1,24 +1,24 @@
-import { and, desc, like } from "drizzle-orm";
-import { db } from "../../../db/drizzle";
-import { todos } from "../../../db/drizzle/schema";
+import { and, desc, like } from 'drizzle-orm'
+import { db } from '../../../db/drizzle'
+import { todos } from '../../../db/drizzle/schema'
 
 type TodoSearchQueryConditions = {
-	title?: string;
-	status?: string;
-};
+  title?: string
+  status?: string
+}
 
 export default async function (query: TodoSearchQueryConditions) {
-	const res = await db
-		.select()
-		.from(todos)
-		.where(
-			and(
-				like(todos.title, `%${query.title ?? ""}%`),
-				like(todos.status, `%${query.status ?? ""}%`),
-			),
-		)
-		.orderBy(desc(todos.id))
-		.execute();
+  const res = await db
+    .select()
+    .from(todos)
+    .where(
+      and(
+        like(todos.title, `%${query.title ?? ''}%`),
+        like(todos.status, `%${query.status ?? ''}%`),
+      ),
+    )
+    .orderBy(desc(todos.id))
+    .execute()
 
-	return res;
+  return res
 }
